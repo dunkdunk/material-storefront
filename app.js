2f38d2376390277810ed179fb0ccab4c03c657ad
Michael Dunkle <michael.dunkle@gmail.com>
2017-05-19 14:49:10 -0600

    Loads mock data dynamically

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-// $(document).ready(function() {
-//   $.getJSON("data/shop.json", function(data) {
-//     console.log(data);
-//   });
-// });
+$(document).ready(function() {
+  const shopData = 'https://raw.githubusercontent.com/dunkdunk/material-storefront/feature/inserts-mock-data/data/shop.json';
+
+  $.getJSON(shopData, function(data) {
+    $('.mdc-toolbar__title').append(data.shop_info.name);
+
+    $.each(data.products, function(i, val) {
+      $('#products').append('<div class="mdc-layout-grid__cell"><div class="mdc-card"><img class="mdc-card__media-item mdc-card__media-item--fit" src="' + val.image + '"></img><section class="mdc-card__primary"><h1 class="mdc-card__title mdc-card__title--large">' + val.title + '</h1><h2 class="mdc-card__subtitle">$' + val.price + '</h2></section></div></div>');
+    });
+
+    $.each(data.categories, function(i, val) {
+      $('#categories').append('<li class="mdc-list-item"><span class="mdc-list-item__text">' + val.name + '<span class="mdc-list-item__text__secondary">' + val.count + ' products</span></span></li>');
+    });
+
+    $.each(data.pages, function(i, val) {
+      $('#pages').append('<li class="mdc-list-item">' + val.title + '</li>');
+    });
+  });
+});
