8fc0c3c98cbeab99546bbb454146c9b5402a89de
Michael Dunkle <michael.dunkle@gmail.com>
2017-06-19 13:50:42 -0600

    Hooks up JSON data

--- app.js
+++ app.js
@@ -42,86 +42,26 @@ Vue.component('cart', {
 })
 
 Vue.component('store-info', {
-  props: ['info'],
-  template: '<span class="mdc-toolbar__title">{{ info.name }}</span>'
+  props: ['shopInfo'],
+  template: '<span class="mdc-toolbar__title">{{ shopInfo.name }}</span>'
 })
 
 var app = new Vue({
   el: '#app',
-  data: {
-    shop_info: {
-      id: 24,
-      name: "My Material Store"
-    },
-    products: [
-      {
-        title: 'My cool shirt',
-        price: '39.99',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: 'https://unsplash.it/200/300/?random',
-        url: '',
-        category: 'Shirts'
-      },
-      {
-        title: 'A nice hat',
-        price: '30',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: 'https://unsplash.it/200/300/?random',
-        url: '',
-        category: 'Hats'
-      },
-      {
-        title: 'Shirt of Gold',
-        price: '199',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: 'https://unsplash.it/200/300/?random',
-        url: '',
-        category: 'Shirts'
-      },
-      {
-        title: 'Snakeskin Socks',
-        price: '49.99',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: 'https://unsplash.it/200/300/?random',
-        url: '',
-        category: 'Socks'
-      },
-      {
-        title: 'A Used Shirt',
-        price: '9',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: 'https://unsplash.it/200/300/?random',
-        url: '',
-        category: 'Shirts'
-      }
-    ],
-    categories: [
-      {
-        name: "Shirts",
-        count: 3
-      },
-      {
-        name: "Socks",
-        count: 2
-      },
-      {
-        name: "Hats",
-        count: 2
-      }
-    ],
-    pages: [
-      {
-        title: "Shipping",
-        slug: "/shipping"
-      },
-      {
-        title: "About us",
-        slug: "/about-us"
-      },
-      {
-        title: "Contact",
-        slug: "/contact"
-      }
-    ]
+  data: () => ({
+    shop: [],
+    errors: []
+  }),
+
+  // Fetches store info when the component is created.
+  created() {
+    axios.get(`https://raw.githubusercontent.com/dunkdunk/material-storefront/master/data/shop.json`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.shop = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
   }
 })
